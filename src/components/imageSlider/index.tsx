c51e934f0259cbb4070ca23c57a609efb875d4c8
Xiang LI <xlil@ctrip.com>
2020-05-17 17:12:43 +0800

    update - init

--- src/components/imageSlider/index.tsx
+++ src/components/imageSlider/index.tsx
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
+import './index.scss';
 
-interface Props {}
-interface State {}
+interface Props {
+  list: Array<string>;
+}
+interface State {
+  items: Array<{
+    id: 'current' | 'left' | 'right';
+    src: string;
+  }>;
+}
 
 export default class ImageSlider extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
+    const { list } = props;
+
+    this.state = {
+      items: [
+        { id: 'left', src: list[0] },
+        { id: 'current', src: list[1] },
+        { id: 'right', src: list[2] },
+      ],
+    };
   }
 
   render() {
-    return <div>ImageSlider</div>;
+    const { items } = this.state;
+    return (
+      <div className="image-slider">
+        {items.map(item => {
+          return (
+            <div key={item.id} className={classnames('image-slider-item', item.id)}>
+              <img src={item.src} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
--- src/components/imageSlider/stories/index.stories.tsx
+++ src/components/imageSlider/stories/index.stories.tsx
@@ -1,8 +1,16 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC } from 'react';
 import ImageSlider from '../index';
 
 export const Wrapper: FC<{}> = () => {
-  return <ImageSlider />;
+  return (
+    <ImageSlider
+      list={[
+        'https://timgsa.baidu.com/timg?image&amp;quality=80&amp;size=b9999_10000&amp;sec=1589711699803&amp;di=421282ba1e8c6cc1eafa50e37156c940&amp;imgtype=0&amp;src=http%3A%2F%2Ft7.baidu.com%2Fit%2Fu%3D2542292652%2C3126886761%26fm%3D193',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1589714568178&di=cd3838096e6370945a4c74de298799ed&imgtype=0&src=http%3A%2F%2Ft7.baidu.com%2Fit%2Fu%3D3634459217%2C3434765490%26fm%3D193',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1589714568178&di=e0b996faa710b0decb1ebc191e483a9e&imgtype=0&src=http%3A%2F%2Ft9.baidu.com%2Fit%2Fu%3D3842172986%2C1245034115%26fm%3D193%26app%3D53%26size%3Dw828%26n%3D0%26g%3D0n%26f%3Djpeg%3Fsec%3D1592296488%26t%3D6dfdde6dbbf10f860d84327e62515b87',
+      ]}
+    />
+  );
 };
 
 export default {
