5d545d557d7630e5826111ecce99e22c4ba7c7d4
Xiang LI <xlil@ctrip.com>
2020-05-17 15:32:51 +0800

    update - storybook

---
+++ src/components/imageSlider/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+interface Props {}
+interface State {}
+
+export default class ImageSlider extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return <div>ImageSlider</div>;
+  }
+}
---
+++ src/components/imageSlider/stories/index.stories.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactNode } from 'react';
+import ImageSlider from '../index';
+
+export const Wrapper: FC<{}> = () => {
+  return <ImageSlider />;
+};
+
+export default {
+  title: 'ImageSlider',
+};
