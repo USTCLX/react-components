dfca7d97bbe20227c4b8831da10fe4902987c57b
Xiang LI <xlil@ctrip.com>
2020-06-03 09:15:30 +0800

    update - image slider ggesture movement

---
+++ src/components/imageSlider/constant.ts
@@ -0,0 +1,1 @@
+export const WIDTH_THRESHOLD = 375 >> 2;
--- src/components/imageSlider/index.tsx
+++ src/components/imageSlider/index.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import { WIDTH_THRESHOLD } from './constant';
 import './index.scss';
 
-type element = 'current' | 'left' | 'right';
+type Action = 'goLeft' | 'goRight' | 'goBack' | 'follow' | 'none';
 
 interface Props {
   list: Array<string>;
@@ -12,52 +13,44 @@ interface State {
     id: 'current' | 'left' | 'right';
     index: number;
   }>;
-  transform: 'none' | 'translateX(-100%)' | 'translateX(100%)';
-  transition: 'none' | 'transform 0.3s ease-in-out';
 }
 
 export default class ImageSlider extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    const { list } = props;
-
     this.state = {
       items: [
         { id: 'left', index: -1 },
         { id: 'current', index: 0 },
         { id: 'right', index: 1 },
       ],
-      transform: 'none',
-      transition: 'none',
     };
   }
 
-  current: number = 0;
-  direction!: 'left' | 'right';
+  private current: number = 0;
+  private direction!: 'left' | 'right' | 'none';
+  private movementX: number = 0;
+  private holding: boolean = false;
+  private listElement = React.createRef<HTMLDivElement>();
 
   handleClickLeft = () => {
     if (this.current > 0) {
       this.direction = 'left';
-      this.setState({
-        transform: 'translateX(100%)',
-        transition: 'transform 0.3s ease-in-out',
-      });
+      this.animator('goLeft');
     }
   };
 
   handleClickRight = () => {
     const { list } = this.props;
-    if (this.current < list.length) {
+    if (this.current < list.length - 1) {
       this.direction = 'right';
-      this.setState({
-        transform: 'translateX(-100%)',
-        transition: 'transform 0.3s ease-in-out',
-      });
+      this.animator('goRight');
     }
   };
 
   handleTransformEnd = () => {
-    const { items, elements, srcs } = this.state;
+    if (this.direction === 'none') return;
+    const { items } = this.state;
     if (this.direction === 'left') {
       this.current--;
       items.push(items.shift()!);
@@ -67,26 +60,88 @@ export default class ImageSlider extends Component<Props, State> {
       items.unshift(items.pop()!);
       items.forEach(item => item.index++);
     }
+    this.animator('none');
     this.setState({
       items,
-      transform: 'none',
-      transition: 'none',
     });
   };
 
+  handleMoveStart = (e: React.MouseEvent<HTMLImageElement>) => {
+    this.holding = true;
+    this.movementX = 0;
+    this.animator('none');
+  };
+
+  handleMoving = (e: React.MouseEvent<HTMLImageElement>) => {
+    if (this.holding) {
+      this.movementX += e.movementX;
+      this.animator('follow', this.movementX);
+    }
+  };
+
+  handleMoveEnd = (e: React.MouseEvent<HTMLImageElement>) => {
+    this.holding = false;
+    this.direction = 'none';
+    if (Math.abs(this.movementX) > WIDTH_THRESHOLD) {
+      if (this.movementX > 0) {
+        this.direction = 'left';
+        this.animator('goLeft');
+      } else {
+        this.direction = 'right';
+        this.animator('goRight');
+      }
+    } else {
+      this.animator('goBack');
+    }
+  };
+
+  animator(action: Action, movementX?: number) {
+    switch (action) {
+      case 'goLeft':
+        this.listElement.current!.style.transform = `translateX(100%)`;
+        this.listElement.current!.style.transition = `transform 0.3s ease-in-out`;
+        break;
+      case 'goRight':
+        this.listElement.current!.style.transform = `translateX(-100%)`;
+        this.listElement.current!.style.transition = `transform 0.3s ease-in-out`;
+        break;
+      case 'goBack':
+        this.listElement.current!.style.transform = `none`;
+        this.listElement.current!.style.transition = `transform 0.3s ease-in-out`;
+        break;
+      case 'follow':
+        this.listElement.current!.style.transform = `translateX(${movementX}px)`;
+        break;
+      case 'none':
+      default:
+        this.listElement.current!.style.transform = `none`;
+        this.listElement.current!.style.transition = `none`;
+        break;
+    }
+  }
+
   render() {
     const { list } = this.props;
-    const { items, transform, transition } = this.state;
+    const { items } = this.state;
     return (
       <div className="image-slider-wrapper">
         <div
           className="image-slider-list"
-          style={{ transform, transition }}
+          ref={this.listElement}
           onTransitionEnd={this.handleTransformEnd}>
           {items.map((item, index) => {
             return (
               <div key={index} className={classnames('image-slider-list-item', item.id)}>
-                <img className={`image-slider-list-item_img`} src={list[item.index]} alt="slider" />
+                <img
+                  className={`image-slider-list-item_img`}
+                  src={list[item.index]}
+                  alt="slider"
+                  draggable={false}
+                  onMouseDown={this.handleMoveStart}
+                  onMouseMove={this.handleMoving}
+                  onMouseUp={this.handleMoveEnd}
+                  onMouseLeave={this.handleMoveEnd}
+                />
               </div>
             );
           })}
